e4bd61cd31da6649de815a0613d791540b49bb48
djpoppa <jpoppa@colostate.edu>
2024-04-29 14:36:47 -0600

    Finish regular level
    Finish regular level

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    public Stopwatch stopwatch; // Reference to the Stopwatch script
+    public delegate void ClickAction();
+    public static event ClickAction OnFinishClicked;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        // Check for mouse button click
-        if (Input.GetMouseButtonDown(0)) // 0 corresponds to the left mouse button
-        {
-            // Cast a ray from the mouse position into the scene
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // Check if the ray hits this object
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
-            {
-                // Check if the stopwatch reference is assigned
-                if (stopwatch != null)
-                {
-                    // Stop the stopwatch
-                    stopwatch.StopStopwatch();
-                }
-            }
-        }
+        OnFinishClicked?.Invoke();
     }
+    
 }
