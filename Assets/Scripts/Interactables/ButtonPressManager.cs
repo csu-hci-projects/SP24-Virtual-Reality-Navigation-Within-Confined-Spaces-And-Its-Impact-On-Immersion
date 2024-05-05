3bee50f96eb8ef2737d5e186498422fdf8713f08
dpoppa4 <dpoppa4@colostate.edu>
2024-05-05 15:16:13 -0600

    Participant Ready

--- Assets/Scripts/Interactables/ButtonPressManager.cs
+++ Assets/Scripts/Interactables/ButtonPressManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class ButtonPressManager : MonoBehaviour
 {
@@ -9,19 +11,55 @@ public class ButtonPressManager : MonoBehaviour
     public Material goldMaterial;
     // This function is called automatically when the GameObject is clicked
     public Material gold2;
-    public GameObject loser;
-    private void OnMouseDown()
-    {
-        // Increment the button press count
-        buttonPressCount++;
+    public GameObject buttonMiddle;
+    private InputAction buttonAction;
+    private float lastTriggerTime = 0f;
+    private float triggerCooldown = 0.5f;
+    public Transform controllerTransform;
+    public GameObject button;
 
-        // Check if the button press count has reached four
-        if (buttonPressCount >= 1)
+    private void Start()
+    {
+        // Enable the lever action
+        buttonAction = new InputAction(binding: "<XRController>/trigger", interactions: "tap");
+        buttonAction.Enable();
+        buttonAction.performed += OnButtonAction;
+    }
+
+    private void OnDisable()
+    {
+        // Disable the lever action when the script is disabled
+        buttonAction.Disable();
+    }
+
+    private void OnButtonAction(InputAction.CallbackContext context)
+    {
+        if (Time.time - lastTriggerTime >= triggerCooldown)
         {
-            // Activate the podium to make it appear
+            lastTriggerTime = Time.time;
+            // Ensure the action only happens once per press
+            // Set the flag to prevent further action until the trigger is released
+            // Handle the lever action
+            RaycastHit hit;
+            if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject == button)
+                {
+                    Debug.Log("Pointing at button");
+                    ChangeButton();
+                }
+            }
+        }
+    }
+
+    private void ChangeButton()
+    {
+        if (buttonPressCount < 1)
+        {
+            buttonPressCount++;
             podium.SetActive(true);
             GetComponent<Renderer>().material = goldMaterial;
-            loser.GetComponent<Renderer>().material = gold2;
+            buttonMiddle.GetComponent<Renderer>().material = gold2;
         }
     }
 } 
